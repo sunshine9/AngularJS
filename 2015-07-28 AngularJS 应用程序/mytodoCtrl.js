852d23d89457d6ea362743af07bff01a12415134
Sun Lifeng <1104506375@qq.com>
2015-12-30 11:12:15 +0800

    之前学习

---
+++ 2015-07-28 AngularJS 应用程序/mytodoCtrl.js
@@ -0,0 +1,13 @@
+﻿app.controller("myTodoCtrl", function ($scope) {
+    $scope.message = "";
+    $scope.left = function () {
+        return 100 - $scope.message.length;
+    };
+    $scope.clear = function () {
+        $scope.message = "";
+    }
+    $scope.save = function () {
+        $scope.message = "";
+        alert("Not Saved!");
+    };
+})
